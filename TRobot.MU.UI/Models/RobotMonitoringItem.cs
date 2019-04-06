24154396b26211a8f3399fa74c77665ac10894ba
mindarchitect <andreas_@ua.fm>
2019-04-06 13:21:18 +0200

    Development

---
+++ TRobot.MU.UI/Models/RobotMonitoringItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TRobot.MU.UI.Models
+{
+    public class RobotMonitoringItem
+    {
+        public LinkedList<Vector> Trajectory { get; set; }
+        public Point CurrentPosition { get; set; }
+        public Color Color { get; set; }
+        public Guid Guid { get; set; }
+        public string Title { get; set; }
+
+    }
+}
--- TRobot.MU.UI/ViewModels/MainWindowViewModel.cs
+++ TRobot.MU.UI/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,22 @@
 ﻿
 using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using TRobot.MU.UI.Models;
 
 namespace TRobot.MU.UI.ViewModels
 {
     public class MainWindowViewModel
     {
-        public IList<object> Robots { get; private set; }
+        public IList<RobotMonitoringItem> Robots { get; private set; }
 
         public MainWindowViewModel()
         {
+            var robotMonitoringItem1 = new RobotMonitoringItem();
+            robotMonitoringItem1.Trajectory = new LinkedList<Vector>();
+            robotMonitoringItem1.Color = Colors.DarkBlue;
+
+            Robots.Add(robotMonitoringItem1);
         }
     }
 }
