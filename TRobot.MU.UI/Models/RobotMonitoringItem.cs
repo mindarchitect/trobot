81ff47001d4d80f118c8612be43a8771776e658d
mindarchitect <andreas_@ua.fm>
2019-04-06 16:09:49 +0200

    Adding MU

--- TRobot.MU.UI/Models/RobotMonitoringItem.cs
+++ TRobot.MU.UI/Models/RobotMonitoringItem.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 
@@ -7,7 +6,8 @@ namespace TRobot.MU.UI.Models
 {
     public class RobotMonitoringItem
     {
-        public LinkedList<Vector> Trajectory { get; set; }
+        public Point StartPoint { get; set; }
+        public PointCollection Trajectory { get; set; }
         public Point CurrentPosition { get; set; }
         public Color Color { get; set; }
         public Guid Guid { get; set; }
--- TRobot.MU.UI/ViewModels/MainWindowViewModel.cs
+++ TRobot.MU.UI/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
@@ -12,9 +13,21 @@ namespace TRobot.MU.UI.ViewModels
 
         public MainWindowViewModel()
         {
+            Robots = new List<RobotMonitoringItem>();
+
+            var robot1Trajectory = new PointCollection();
+
+            robot1Trajectory.Add(new Point(10, 10));
+            robot1Trajectory.Add(new Point(20, 40));
+            robot1Trajectory.Add(new Point(40, 70));            
+
             var robotMonitoringItem1 = new RobotMonitoringItem();
-            robotMonitoringItem1.Trajectory = new LinkedList<Vector>();
+            robotMonitoringItem1.StartPoint = new Point(0, 0);
+            robotMonitoringItem1.Trajectory = robot1Trajectory;
             robotMonitoringItem1.Color = Colors.DarkBlue;
+            robotMonitoringItem1.CurrentPosition = new Point(30, 50);
+            robotMonitoringItem1.Guid = Guid.NewGuid();
+            robotMonitoringItem1.Title = "Robot 1";            
 
             Robots.Add(robotMonitoringItem1);
         }
