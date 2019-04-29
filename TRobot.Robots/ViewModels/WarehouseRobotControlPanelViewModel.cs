fd87ffa8b75dd8515b3bd192913be92cd88e0b70
Andriy Lavrusha <alavrusha@gk-software.com>
2019-04-29 13:36:52 +0200

    sdfsf

--- TRobot.Robots/ViewModels/WarehouseRobotControlPanelViewModel.cs
+++ TRobot.Robots/ViewModels/WarehouseRobotControlPanelViewModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Threading;
 using TRobot.Core;
 using TRobot.Core.Enums;
 using TRobot.Core.UI.Commands;
